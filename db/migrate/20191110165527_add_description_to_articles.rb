8394ee22d2f3b45c8a409bdc0432c42c9231a343
Levent Ince <levent.ince@shopify.com>
2019-11-10 13:01:22 -0500

    Create the application and add the first table (article)

---
+++ db/migrate/20191110165527_add_description_to_articles.rb
@@ -0,0 +1,6 @@
+ class AddDescriptionToArticles < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :description, :text
+    add_column :articles, :is_active, :boolean
+  end
+end
