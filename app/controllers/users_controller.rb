2d8bfdee45a7d9b7e9904d80e4437cc856307208
Levent Ince <levent.ince@shopify.com>
2019-11-12 13:34:08 -0500

    implemen signup user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,88 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
+  # GET /articles.json
+
+  def index
+    @users = User.all
+  end
+
+  # GET /articles/1
+  # GET /articles/1.json
+  def show
+    #
+    # id = params[:id].to_i
+    # id = id -8
+    # @article = Article.find(id)
+     # @article = Article.find(params[:id])
+
+  end
+
+  # GET /articles/new
+  def new
+     @user = User.new
+  end
+
+  # GET /articles/1/edit
+  def edit
+  end
+
+  # POST /articles
+  # POST /articles.json
+  def create
+
+
+
+    @user = User.new(user_params)
+
+
+    respond_to do |format|
+      if @user.save
+
+         format.html { redirect_to @user , notice: 'User was successfully created.' }
+        format.json { render :action => "show", status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  # PATCH/PUT /articles/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /articles/1
+  # DELETE /articles/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      # format.html { redirect_to articles_url }
+       format.html { redirect_to user_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'welcome/about'
   resources :articles
   root 'welcome#home'
+
+  get 'signup', to: 'users#new'
+  resources :users  #, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
